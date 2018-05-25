66cf516ae0daa8871f6ecf5ffab2be77d4b3150d
sandeep1456 <kamblesandeepr@gmail.com>
2018-05-25 19:00:53 +0530

    Introduce Redux reducer

---
+++ src/reducers/matches.js
@@ -0,0 +1,22 @@
+let defaultState = [
+  {
+    "mDate" : "2018-05-02",
+    "mTime" : "9:00 AM",
+    "tournament" : "Pune IT Cup",
+    "ground" : "Shinde High School",
+    "team1" : "Synerzip",
+    "team2" : "Zensar",
+    "t1Score" : 180,
+    "t2score" : 156,
+    "t1wickets" : 4,
+    "t2wickets" : 10,
+    "winner" : "Synerzip",
+    "mom" : "Amey"
+  }
+];
+
+const matches = (state=defaultState, action) => {
+  return state;
+}
+
+export default matches;
