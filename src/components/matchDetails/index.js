01ff680efec881f59a155f6ab2b27a17ea4da058
sandeep1456 <kamblesandeepr@gmail.com>
2018-06-07 13:21:30 +0530

    Placeholder for Batting and Bolwing details

--- src/components/matchDetails/index.js
+++ src/components/matchDetails/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
+import { Grid, Row, Col, PageHeader, Panel, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class MatchDetails extends Component {
@@ -12,6 +12,8 @@ class MatchDetails extends Component {
       return <div>Please select a Match</div>
     }
 
+    const battingDetails = [1,2,3,4,5,6,7,8,9,10];
+    const bowlingDetails = [1,2,3,4,5];
     return (
       <Grid>
         <Row>
@@ -20,9 +22,39 @@ class MatchDetails extends Component {
           </PageHeader>
         </Row>
         <br/>
-        <Row>
-          <Col md={6} className='list-group'>
-            <Row className='list-group-item'>
+        <Row className="no-gutters">
+          <Col md={2} className='inning-box list-group text-center'>
+            <Row className='list-group-item active no-gutters'>
+              <Col md={12}> First Inning </Col>
+            </Row>
+            <Row className='list-group-item  no-gutters'>
+              <Col md={12}>
+                <Row>
+                  <Col md={12}><h4>{this.props.matchData.t1Name}</h4></Col>
+                  <Col md={12}><h1>{this.props.matchData.t1Score}/{this.props.matchData.t1wickets}</h1></Col>
+                  <Col md={12}>{this.props.matchData.t1Overs} overs</Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col md={2} className='inning-box list-group text-center '>
+            <Row className='list-group-item active no-gutters'>
+              <Col md={12}> Second Inning </Col>
+            </Row>
+            <Row className='list-group-item  no-gutters'>
+              <Col md={12}>
+                <Row>
+                  <Col md={12}><h4>{this.props.matchData.t2Name}</h4></Col>
+                  <Col md={12}><h1>{this.props.matchData.t2Score}/{this.props.matchData.t2wickets}</h1></Col>
+                  <Col md={12}>{this.props.matchData.t2Overs} overs</Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col md={8} className='list-group'>
+            <Row className='list-group-item no-gutters'>
               <Col md={6}>
                 <Row>
                   <Col md={4}><strong>Tournament</strong></Col>
@@ -36,7 +68,7 @@ class MatchDetails extends Component {
                 </Row>
               </Col>
             </Row>
-            <Row className='list-group-item'>
+            <Row className='list-group-item no-gutters'>
               <Col md={6}>
                 <Row>
                   <Col md={4}><strong>Date</strong></Col>
@@ -50,7 +82,7 @@ class MatchDetails extends Component {
                 </Row>
               </Col>
             </Row>
-            <Row className='list-group-item'>
+            <Row className='list-group-item no-gutters'>
               <Col md={6}>
                 <Row>
                   <Col md={4}><strong>Result</strong></Col>
@@ -64,7 +96,7 @@ class MatchDetails extends Component {
                 </Row>
               </Col>
             </Row>
-            <Row className='list-group-item'>
+            <Row className='list-group-item no-gutters'>
               <Col md={12}>
                 <strong>Toss</strong> {' '}
                 Won by {' '} <strong>{this.props.matchData.toss}</strong> {' '}
@@ -72,11 +104,93 @@ class MatchDetails extends Component {
               </Col>
             </Row>
           </Col>
+        </Row>
+        <br/>
 
+        {/* batting details */}
+        <Row>
           <Col md={6}>
+            <Panel bsStyle="primary">
+              <Panel.Heading>
+                <Panel.Title componentClass="h3">Synerzip Batting</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Runs</th>
+                      <th>Balls</th>
+                      <th>4s</th>
+                      <th>6s</th>
+                      <th>S/R</th>
+                      <th>Dismissal</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {battingDetails.map((record, index) => {
+                    return(
+                      <tr key={index}>
+                        <td>{record}</td>
+                        <td>Bastman {record}</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                      </tr>
+                    )
+                  })}
+                  </tbody>
+                </Table>
+              </Panel.Body>
+              <Panel.Footer>Extras: 0</Panel.Footer>
+            </Panel>
           </Col>
         </Row>
+        <br/>
 
+        {/* bowling details */}
+        <Row>
+          <Col md={6}>
+            <Panel bsStyle="primary">
+              <Panel.Heading>
+                <Panel.Title componentClass="h3">Synerzip Bowling</Panel.Title>
+              </Panel.Heading>
+              <Panel.Body>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Overs</th>
+                      <th>Runs</th>
+                      <th>Wickets</th>
+                      <th>Economy</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {bowlingDetails.map((record, index) => {
+                    return(
+                      <tr key={index}>
+                        <td>{record}</td>
+                        <td>Bowler {record}</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                      </tr>
+                    )
+                  })}
+                  </tbody>
+                </Table>
+              </Panel.Body>
+              <Panel.Footer>Extras: 0</Panel.Footer>
+            </Panel>
+          </Col>
+        </Row>
       </Grid>
     )
   }
